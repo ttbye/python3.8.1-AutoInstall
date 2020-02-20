a84ba1db2736090c4b4b86babdce02f58020a1f1
ttbye <ttbye@126.com>
2020-02-20 21:37:51 +0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,13 +3,18 @@
 # Author: ttbye
 #########################################################################
 #!/usr/bin/env bash
-yum -y install gcc &&
+#1. gcc相关组件
+yum install make gcc gcc-c++ &&
+#2. zlib的解压缩类库
+yum -y install zlib* &&
+#支持python退格的组件
+yum install readline-devel &&
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
 wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz &&
-mkdir -p /usr/local/python3.8 &&
+mkdir -p /usr/local/python3 &&
 tar -xvf Python-3.8.1.tgz &&
 cd Python-3.8.1 &&
-./configure --prefix=/usr/local/python3.8 &&
+./configure --prefix=/usr/local/python3 &&
 make &&
 make install &&
 ln -s /usr/local/python3/bin/python3.8 /usr/local/bin/python3 &&
